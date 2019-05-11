46d30168f7b46e65d85f773b950de64cbed0f1a6
Sebastian Lubiecki <sebastian.lubiecki@gmail.com>
2019-05-11 12:26:08 +0200

    Added implementation of models Author and Quiz classes

--- src/main/java/pl/brainstorm/question/Models/Author.java
+++ src/main/java/pl/brainstorm/question/Models/Author.java
@@ -1,5 +1,75 @@
 package pl.brainstorm.question.Models;
 
+import java.util.List;
+
 public class Author {
 
+    private Long id;
+    private String name;
+    private String surname;
+    private String email;
+    private List<Question> quizList;
+
+    public Author() {
+    }
+
+    public Author(Long id, String name, String surname, String email, List<Question> quizList) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.quizList = quizList;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Question> getQuizList() {
+        return quizList;
+    }
+
+    public void setQuizList(List<Question> quizList) {
+        this.quizList = quizList;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", email='" + email + '\'' +
+                ", quizList=" + quizList +
+                '}';
+    }
 }
--- src/main/java/pl/brainstorm/question/Models/Quiz.java
+++ src/main/java/pl/brainstorm/question/Models/Quiz.java
@@ -1,4 +1,53 @@
 package pl.brainstorm.question.Models;
 
+import java.util.List;
+
 public class Quiz {
+
+
+    private Author author;
+    private List<Question> questionsList;
+    private Long totalScore;
+
+    public Quiz() {
+    }
+
+    public Quiz(Author author, List<Question> questionsList, Long totalScore) {
+        this.author = author;
+        this.questionsList = questionsList;
+        this.totalScore = totalScore;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public List<Question> getQuestionsList() {
+        return questionsList;
+    }
+
+    public void setQuestionsList(List<Question> questionsList) {
+        this.questionsList = questionsList;
+    }
+
+    public Long getTotalScore() {
+        return totalScore;
+    }
+
+    public void setTotalScore(Long totalScore) {
+        this.totalScore = totalScore;
+    }
+
+    @Override
+    public String toString() {
+        return "Quiz{" +
+                "author=" + author +
+                ", questionsList=" + questionsList +
+                ", totalScore=" + totalScore +
+                '}';
+    }
 }
