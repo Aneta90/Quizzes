6b5297c5e2dba14e34ee0cb78d87abe632c65508
Aneta <aw22@onet.pl>
2019-05-11 12:28:03 +0200

    Added implementation of models: Question and Response classes!

--- src/main/java/pl/brainstorm/question/Models/Question.java
+++ src/main/java/pl/brainstorm/question/Models/Question.java
@@ -1,4 +1,57 @@
 package pl.brainstorm.question.Models;
 
+import pl.brainstorm.question.Domain.Entities.QuizEntity;
+import pl.brainstorm.question.Domain.Entities.ResponseEntity;
+
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.util.List;
+
 public class Question {
+
+    private QuizEntity quizEntity;
+    private String content;
+    private List<ResponseEntity> responseEntityList;
+
+    public Question() {
+    }
+
+    public Question(QuizEntity quizEntity, String content, List<ResponseEntity> responseEntityList) {
+        this.quizEntity = quizEntity;
+        this.content = content;
+        this.responseEntityList = responseEntityList;
+    }
+
+    public QuizEntity getQuizEntity() {
+        return quizEntity;
+    }
+
+    public void setQuizEntity(QuizEntity quizEntity) {
+        this.quizEntity = quizEntity;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public List<ResponseEntity> getResponseEntityList() {
+        return responseEntityList;
+    }
+
+    public void setResponseEntityList(List<ResponseEntity> responseEntityList) {
+        this.responseEntityList = responseEntityList;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "quizEntity=" + quizEntity +
+                ", content='" + content + '\'' +
+                ", responseEntityList=" + responseEntityList +
+                '}';
+    }
 }
--- src/main/java/pl/brainstorm/question/Models/Response.java
+++ src/main/java/pl/brainstorm/question/Models/Response.java
@@ -1,4 +1,119 @@
 package pl.brainstorm.question.Models;
 
+import pl.brainstorm.question.Domain.Entities.QuestionsEntity;
+
 public class Response {
+
+    private QuestionsEntity questions;
+
+    private String answerA;
+    private String answerB;
+    private String answerC;
+    private String answerD;
+    private Boolean isACorrect;
+    private Boolean isBCorrect;
+    private Boolean isCCorrect;
+    private Boolean isDCorrect;
+
+    public Response() {
+    }
+
+    public Response(QuestionsEntity questions, String answerA, String answerB, String answerC, String answerD, Boolean isACorrect, Boolean isBCorrect, Boolean isCCorrect, Boolean isDCorrect) {
+        this.questions = questions;
+        this.answerA = answerA;
+        this.answerB = answerB;
+        this.answerC = answerC;
+        this.answerD = answerD;
+        this.isACorrect = isACorrect;
+        this.isBCorrect = isBCorrect;
+        this.isCCorrect = isCCorrect;
+        this.isDCorrect = isDCorrect;
+    }
+
+    public QuestionsEntity getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(QuestionsEntity questions) {
+        this.questions = questions;
+    }
+
+    public String getAnswerA() {
+        return answerA;
+    }
+
+    public void setAnswerA(String answerA) {
+        this.answerA = answerA;
+    }
+
+    public String getAnswerB() {
+        return answerB;
+    }
+
+    public void setAnswerB(String answerB) {
+        this.answerB = answerB;
+    }
+
+    public String getAnswerC() {
+        return answerC;
+    }
+
+    public void setAnswerC(String answerC) {
+        this.answerC = answerC;
+    }
+
+    public String getAnswerD() {
+        return answerD;
+    }
+
+    public void setAnswerD(String answerD) {
+        this.answerD = answerD;
+    }
+
+    public Boolean getACorrect() {
+        return isACorrect;
+    }
+
+    public void setACorrect(Boolean ACorrect) {
+        isACorrect = ACorrect;
+    }
+
+    public Boolean getBCorrect() {
+        return isBCorrect;
+    }
+
+    public void setBCorrect(Boolean BCorrect) {
+        isBCorrect = BCorrect;
+    }
+
+    public Boolean getCCorrect() {
+        return isCCorrect;
+    }
+
+    public void setCCorrect(Boolean CCorrect) {
+        isCCorrect = CCorrect;
+    }
+
+    public Boolean getDCorrect() {
+        return isDCorrect;
+    }
+
+    public void setDCorrect(Boolean DCorrect) {
+        isDCorrect = DCorrect;
+    }
+
+    @Override
+    public String toString() {
+        return "Response{" +
+                "questions=" + questions +
+                ", answerA='" + answerA + '\'' +
+                ", answerB='" + answerB + '\'' +
+                ", answerC='" + answerC + '\'' +
+                ", answerD='" + answerD + '\'' +
+                ", isACorrect=" + isACorrect +
+                ", isBCorrect=" + isBCorrect +
+                ", isCCorrect=" + isCCorrect +
+                ", isDCorrect=" + isDCorrect +
+                '}';
+    }
 }
