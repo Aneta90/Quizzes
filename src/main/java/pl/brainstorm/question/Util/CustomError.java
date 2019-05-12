1edc3a705e017e23861cf035900afc7176a70048
Aneta <aw22@onet.pl>
2019-05-12 11:24:16 +0200

    Question Controller added!

---
+++ src/main/java/pl/brainstorm/question/Util/CustomError.java
@@ -0,0 +1,15 @@
+package pl.brainstorm.question.Util;
+
+public class CustomError {
+
+        private String errorMassage;
+
+        public CustomError(String errorMassage) {
+            this.errorMassage = errorMassage;
+        }
+
+        public String getErrorMassage() {
+            return errorMassage;
+        }
+    }
+
