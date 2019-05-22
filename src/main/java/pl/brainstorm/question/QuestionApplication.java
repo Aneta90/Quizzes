9ea8537bde19086c021322fc771f5abc6ddcef30
Aneta <aw22@onet.pl>
2019-05-22 12:12:44 +0200

    log4j2.xml added, logs/app.log add automatically for saving logs

--- src/main/java/pl/brainstorm/question/QuestionApplication.java
+++ src/main/java/pl/brainstorm/question/QuestionApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class QuestionApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(QuestionApplication.class, args);
 	}
